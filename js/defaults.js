4dbfd3c4d5fbb86357ba815b149e4bd298898503
Jacob Alexander <haata@kiibohd.com>
2015-10-01 22:37:25 -0700

    Removing text editor key
    - Not functional on Linux (not holding my breath for the other OSs)

--- js/defaults.js
+++ js/defaults.js
@@ -154,7 +154,6 @@ APP.keyDefaults = {
 'CONS:CALCULATOR': { y: 0, x: 0, w: 4, h: 4, label: 'CALC', group: 'multimedia' },
 'CONS:INTERNETBROWSER': { y: 0, x: 0, w: 4, h: 4, label: 'WEB', group: 'multimedia' },
 'CONS:FILEBROWSER': { y: 0, x: 0, w: 4, h: 4, label: 'FILES', group: 'multimedia' },
-'CONS:TEXTEDITOR': { y: 0, x: 0, w: 4, h: 4, label: 'EDITOR', group: 'multimedia' },
 
 'SYS:POWERDOWN': { y: 0, x: 0, w: 4, h: 4, label: 'POWER', group: 'multimedia' },
 'SYS:SLEEP': { y: 0, x: 0, w: 4, h: 4, label: 'SLEEP', group: 'multimedia' },
