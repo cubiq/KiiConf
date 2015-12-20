56538c1ac2d75cf3a019c79e7c125a168985a8df
Matteo Spinelli <matteo@cubiq.org>
2015-12-20 08:50:33 +0100

    fix link to the wiki

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		<select id="layout-list"><?php layoutList() ?></select>
 		<button type="button" id="load-layout" class="button-read">load layout</button>
 		<button type="button" id="import-map" class="button-read">import map</button>
-		<input type="button" onclick="location.href='https://github.com/kiibohd/KiiConf';" value="Wiki" />
+		<input type="button" onclick="window.top.location.href='https://github.com/kiibohd/KiiConf';" value="Wiki" />
 		<input type="button" onclick="location.href='stats.json';" value="Version" />
 		<button type="button" id="download-map" class="button-write floatright">download firmware</button>
 	</nav>
